69f91d8a2ba1ef1fd6ba7f2a76b12b19ce87f6a2
KevinChavezIT <hc7657819@gmail.com>
2021-07-20 23:53:01 -0500

    lista personas

---
+++ Crud/Controllers/UsuariosController.cs
@@ -0,0 +1,158 @@
+﻿using Crud.Data;
+using Crud.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud.Controllers
+{
+    public class UsuariosController : Controller
+    {
+
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public UsuariosController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+
+        }
+        public IActionResult Index()
+        {
+            List<Persona> usarios = new List<Persona>();
+            usarios = _applicationDbContext.Persona.ToList();
+
+            return View(usarios);
+        }
+        public IActionResult Details(int id)
+        {
+            if (id== 0)
+                return RedirectToAction("Index");
+
+            Persona persona = _applicationDbContext.Persona.Where(p => p.Codigo == id).FirstOrDefault();
+            if (persona==null)
+                return RedirectToAction("Index");
+
+            return View(persona);
+        }
+        public IActionResult Create()
+        {     
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Persona persona)
+        {
+            try
+            {
+                persona.Estado = 1;
+
+             _applicationDbContext.Add(persona);
+            _applicationDbContext.SaveChanges();
+            }
+            catch(Exception)
+            {
+                return View(persona);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            if(id == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Persona persona = _applicationDbContext.Persona.Where(p => p.Codigo == id).FirstOrDefault();
+            
+            if(persona==null)
+                return RedirectToAction("Index");
+
+            return View(persona);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Persona persona)
+        {
+            if (id != persona.Codigo)
+                return RedirectToAction("Index");
+
+            try
+            {               
+                _applicationDbContext.Update(persona);
+                _applicationDbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                return View(persona);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Persona persona = _applicationDbContext.Persona.Where(p => p.Codigo == id).FirstOrDefault();
+
+            try
+            {
+                _applicationDbContext.Remove(persona);
+                _applicationDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Desactivar(int id)
+        {
+            if (id == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Persona persona = _applicationDbContext.Persona.Where(p => p.Codigo == id).FirstOrDefault();
+
+            try
+            {
+                persona.Estado = 0;
+
+                _applicationDbContext.Update(persona);
+                _applicationDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Activar(int id)
+        {
+            if (id == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Persona persona = _applicationDbContext.Persona.Where(p => p.Codigo == id).FirstOrDefault();
+
+            try
+            {
+                persona.Estado = 1;
+
+                _applicationDbContext.Update(persona);
+                _applicationDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Crud.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crud.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Persona>Persona{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Persona>(en => {
+
+                en.HasKey(p => p.Codigo);
+
+                en.Property(p => p.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+                en.Property(p => p.Apellido)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+                en.Property(p => p.Direccion)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+                en.Property(p => p.Estado)             
+               .IsUnicode(false);
+
+            });
+        }
+    }
+}
